2e11e7661c993f952d6eeca87b471fad65d5c6ac
Difei (Amber) Gao <dg3294@nyu.edu>
2023-10-17 23:27:26 -0400

    Week 2

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # Book Choice
 I chose The Merlin Trilogy as my book. This is a book about a series of three historical fantasy novels that reimagines the Arthurian legend through the perspective of the legendary wizard Merlin. 
 
+
+# Week 2 update
+This week I tried to make the sword animation. Somehow it has this feature of lifting to the sky. It is pretty cool, but I'll refine it in the future. Also, I used RGB dots to try to create this sky of stars. For now, I have functions of drawing stars and the sword. For the following week, I would:
+
+1. Use smaller dots to create a high-detailed sky (for loop) - can I use for loop to cover every (1,1) pixels?
+2. Create a pixel-style stone to cover up the sword
+3. Use color change based on second to do time-based starry sky
+4. Create a giant "+" Star in the bg using for loop
+5. Use class() to define some decorative elements
+6. Create a title using text()
+
+
+
+
 ## Book Cover
 1. I'll include Merlin the wizard in book cover. 
 2. Stars (gifs)
--- sketch.js
+++ sketch.js
@@ -1,6 +1,63 @@
+let swordX;
+let swordY;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(0)
+  swordX = width / 2;
+  swordY = height;
+  drawFlowers();
 }
 
 function draw() {
+  // text("The Merlin Trilogy ", x, y, x2, y2)
+  drawSword();
+  moveSword();
+}
+
+function drawSword() {
+  // Blade
+  fill("yellow");
+  rect(swordX - 5, swordY, 20, 60);
+  rect(swordX - 5, swordY - 20, 20, 60);
+  rect(swordX - 5, swordY + 30, 20, 60);
+  triangle(swordX - 30, swordY, swordX + 40, swordY, swordX + 5, swordY - 40);
+  
+
+  // Hilt
+  fill(100);
+  rect(swordX - 25, swordY + 60, 60, 10);
+}
+
+function moveSword() {
+  if (swordY > 0) {
+    swordY -= 2;
+  } else {
+    swordY = height;
+  }
+
+}
+
+function drawFlowers() {
+  for (let i = 0; i < width; i += 10) {
+    for (let j = 0; j < height; j += 10) {
+      drawFlower(i, j);
+    }
+  }
+}
+
+function drawFlower(x, y) {
+  // Draw flower indots
+  noStroke();
+  fill("pink");
+  ellipse(random(width), random(height), 2, 2);
+
+  fill("purple");
+  ellipse(random(width), random(height), 2, 2);
+
+  fill("yellow");
+  ellipse(random(width), random(height), 2, 2);
+
+  fill("green");
+  ellipse(random(width), random(height), 2, 2);
 }
