9cf64720764b5440247a97be6fd58d5db932c572
Difei (Amber) Gao <dg3294@nyu.edu>
2023-10-24 21:57:25 -0400

    Final

--- README.md
+++ README.md
@@ -1,5 +1,72 @@
+# Guide
+Book: The Merlin Trilogy  
+Intro: An authorian legend but from the perspective of Wizard Merlin  
+Ways of Interactivity: Time-based animation + Click to generate stars in the sky!  
+
 # Book Choice
 I chose The Merlin Trilogy as my book. This is a book about a series of three historical fantasy novels that reimagines the Arthurian legend through the perspective of the legendary wizard Merlin. 
+
+
+
+# Week 3 Final Update
+This week, I did a lot by rethinking what a book cover should be.   
+First, I ditched the original way of drawing dots using only random(). I chose to try for() to create dots in a more controllable way.I created the background like this:
+![image description](./5.png)  
+
+I added random() to add onto the variables to make the grid shifts.  
+![image description](./6.png)  
+
+
+I observed that the dots are too uniformly distributed and it's not like an actual sky, so I tried to control the density of dots by adjusting the spacing in between and the size of circles. I noticed that when the spacing is too small, the framerate can't be displayed as fast as a simpler one.   
+![image description](./4.png)  
+
+Then, I moved on to the front scene. I planned to use the actual ratio of book to draw the new book cover, so I got the number 6.4" x 9.6". Because the book size is bigger than my screen, I decided to use ratio. I used the way how we put the rectangle in the middle of image by using the width/2 - length/2. 
+![image description](./7.png)  
+![image description](./8.png)  
+
+
+After having an actual cover, I began considering resketching the whole cover. I went to my Ipad to draw this:
+![image description](./9.png)  
+
+To draw the pink area, I struggled to find tutorials or references for drawing solid curves with specific vertex locations and angle at the same time. This is a paragraph I tried:  
+![image description](./11.png)  
+
+
+ So I thought about directly using images like we just learned in class. I drew this on my ipad using a high dpi and approximate height reserved for it:
+![image description](./12.png) 
+![image description](./10.png) 
+
+I managed to adjust the size and position of the images.   
+![image description](./13.png) 
+
+In the meantime, I wrote a lot of function() to draw different parts. It was a realization how I didn't need to do all the stuffs in draw().  
+
+Then, I moved on to compose Author's Excalibri rising up from the stone. I first corrected the form of sword to a more pleasant way by using beginshape(). Then I revised the original moveSword() to make the sword move using if else. 
+![image description](./14.png) 
+
+
+I wanted to compose a light when the sword comes out (stones turn out to be too tedious). So I wrote a drawOrna() function using if/else to make a loop for the ellipse. I added the author's name, Mary Stewart. 
+![image description](./15.png) 
+
+
+Finally, I wanted to add some big stars to the cover. I wanted it to be interactive, so I created a star class and refered to stars on Processing (I couldn't do it) to create a four-corner star. In the class, I wrote the update of alpha channel and outside a mouseClicked(). 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # Week 2 update
--- sketch.js
+++ sketch.js
@@ -11,14 +11,24 @@ let dotSpacing = 4;
 let iOffset = 0;
 let jOffset = 0;
 
+
+
 let myFont;
 function preload() {
   myFont = loadFont('NeueMontreal-Bold.otf');
+  mCamera = loadImage("./2.png")
+  mCameraB = loadImage("./3.png")
 }
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  pixelDensity(2);
+  mCamera.resize(0, 500);
+  mCameraB.resize(0, 500)
+  mCamera.loadPixels();
+  mCameraB.loadPixels();
+
   swordX = width / 2;
   swordY = height;
 
@@ -34,19 +44,28 @@ function drawCover(){
   let bookWidth = bookHeight/9.6 * 6.4;
   fill("pink")
   rect(width/2 - bookWidth/2, 0, bookWidth, bookHeight);
+  print(bookWidth)
 
 }
 
-function drawOrna(){
-  fill(255, random(255), random(255)); 
-	beginShape();
-	    for(let i=0; i<20; i++){
-					
-				curveVertex(random (0,width), random(0, height));
-			}
-	
-	endShape(CLOSE);
+let rOrna = 100; 
+let maxROrna = 400; 
+let growing = true; // A flag to indicate whether the ellipse is growing or shrinking
 
+function drawOrna() {
+  if (growing) {
+    rOrna += 4; 
+    if (rOrna >= maxROrna) {
+      growing = false; // Set the flag to false when the maximum size is reached
+    }
+  } else {
+    rOrna -= 4; 
+    if (rOrna <= 100) {
+      growing = true; // Set the flag to true when the ellipse returns to its initial size
+    }
+  }
+  fill(255, random(255), random(255));
+  ellipse(width / 2, height, rOrna);
 }
 
 function drawFlower() {
@@ -65,11 +84,18 @@ function drawFlower() {
 function drawTitle(){
   textAlign(CENTER);
   textSize(120);
-  fill(cPurple);
+  fill(cRed);
   textFont(myFont);
+  text("THE", width/2 + 4, height/3 - 110 + 4)
+  text("MERLIN", width/2 + 4, height/3 + 4);
+  text("TRILOGY", width/2 + 4, height/3 + 110 + 4);
+  fill(cPurple);
   text("THE", width/2, height/3 - 110)
   text("MERLIN", width/2, height/3);
   text("TRILOGY", width/2, height/3 + 110);
+
+  textSize(20)
+  text("MARY STEWART", width/2, height - 100)
 }
 
 function drawStone(){
@@ -77,16 +103,36 @@ function drawStone(){
 }
 
 
+
+
+
+let cameraXOffset = -10; 
+let cameraYOffset = 370;
+// let cameraX2Offset = screen.width/2 - 550 + screen.height/9.6 * 6.4; 
+// let cameraY2Offset = 220;
+let cameraX2Offset = screen.width/2 - 370+ screen.height/9.6 * 6.4; 
+let cameraY2Offset = 370;
+
 function draw() {
   // Move the background dots down every second
   if (frameCount % 60 == 0) {
     jOffset += dotSpacing;
   }
   background(20);
-  drawCover();
+  mCamera.loadPixels();
+  image(mCamera, cameraXOffset, cameraYOffset);
+  image(mCameraB, cameraX2Offset, cameraY2Offset)
+
   drawFlower();
-  drawTitle();
+  stars.forEach(star => {
+    star.display();
+    star.update();
+  });
+
+  drawCover();  
   drawOrna();
+  drawTitle();
+
 
   drawSword();
   moveSword();
@@ -94,21 +140,82 @@ function draw() {
 
 function drawSword() {
   // Blade
-  fill("yellow");
-  rect(swordX - 5, swordY, 20, 60);
-  rect(swordX - 5, swordY - 20, 20, 60);
-  rect(swordX - 5, swordY + 30, 20, 60);
-  triangle(swordX - 30, swordY, swordX + 40, swordY, swordX + 5, swordY - 40);
+  fill("gray");
+  beginShape();
+  let ratio = 1.5; // Adjust this value to change the size of the sword
+  let hiltWidth = 40 * ratio;
+  let hiltHeight = 10 * ratio;
+  let bladeTopX = swordX + 10 * ratio;
+  let bladeTopY = swordY - 60 * ratio;
+  let bladeMiddleX = swordX;
+  let bladeMiddleY = swordY - 120 * ratio;
+  let bladeBottomX = swordX - 10 * ratio;
+  let bladeBottomY = swordY - 60 * ratio;
+
+  vertex(swordX - 5 * ratio, swordY);
+  vertex(swordX + 5 * ratio, swordY);
+  vertex(bladeTopX, bladeTopY);
+  vertex(bladeMiddleX, bladeMiddleY);
+  vertex(bladeBottomX, bladeBottomY);
+  endShape(CLOSE);
 
   // Hilt
   fill(100);
-  rect(swordX - 25, swordY + 60, 60, 10);
+  rect(swordX - hiltWidth / 2, swordY, hiltWidth, hiltHeight);
+  rect(swordX - 7.5 * ratio, swordY, 15 * ratio, 45 * ratio);
 }
 
 function moveSword() {
-  if (swordY > 0) {
-    swordY -= 2;
+  let ratio = 1.5; 
+  if (swordY > -120 * ratio) {
+    swordY -= 2 * ratio;
   } else {
-    swordY = height - 60;
+    swordY = height;
   }
+}
+
+let stars = [];
+function mouseClicked() {
+  stars.push(new Star(mouseX, mouseY, 100, random(50, 255), 60, 2));
+}
+
+class Star {
+  constructor(x, y, radius, alpha, lifeSpan, fadeSpeed) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.alpha = alpha;
+    this.lifeSpan = lifeSpan;
+    this.fadeSpeed = fadeSpeed;
+  }
+
+  display() {
+    fill(random(255), this.alpha);
+    noStroke();
+    this.drawStar(this.x, this.y, this.radius/2, this.radius / 10, 4);
+  }
+  update() {
+    this.lifeSpan--;
+    this.alpha = this.lifeSpan * this.fadeSpeed;
+    if (this.lifeSpan <= 0) {
+      stars.shift();
+    }
+  }
+
+  drawStar(x, y, radius1, radius2, npoints) {
+    let angle = TWO_PI / npoints;
+    let halfAngle = angle / 2.0;
+    beginShape();
+    for (let a = -PI / 4; a < TWO_PI; a += angle) {
+      let sx = x + cos(a) * radius2;
+      let sy = y + sin(a) * radius2;
+      vertex(sx, sy);
+      sx = x + cos(a + halfAngle) * radius1;
+      sy = y + sin(a + halfAngle) * radius1;
+      vertex(sx, sy);
+    }
+    endShape(CLOSE);
+  }
+
+
 }
